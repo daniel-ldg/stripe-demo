164a049cb1b1c165a5af796c37e2a4abcaa97c6a
daniel-ldg <daniel.ldg@icloud.com>
2021-11-08 13:20:47 -0600

    video1

---
+++ servidor/stripe.php
@@ -0,0 +1,5 @@
+<?php
+
+include __DIR__ . '/stripe-php-master/init.php';
+
+$stripe = new \Stripe\StripeClient("sk_test_51JF5DuEhw8eLdPcfJpbTzrktPr4E0AjCmnaWW2NQ7Naomsq01e2VxPxcw8izh4nfXb4R76ZEX4fEV9w5zhS1y7rd00PVYKZoMB");
